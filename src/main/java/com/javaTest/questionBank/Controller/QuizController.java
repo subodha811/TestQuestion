e1625110dcfa14d39f05fb0986aa3fda2898c7d4
Subodha Pradhan <subodhapradhan811@gmail.com>
2024-08-16 07:14:02 +0530

    Added changes

--- src/main/java/com/javaTest/questionBank/Controller/QuizController.java
+++ src/main/java/com/javaTest/questionBank/Controller/QuizController.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaTest.questionBank.Model.Question;
+import com.javaTest.questionBank.Model.QuestionWapper;
 import com.javaTest.questionBank.Service.QuizService;
 
 @RestController
@@ -25,5 +28,11 @@ public class QuizController {
 	public ResponseEntity<String> getQuiz(@RequestParam String category,@RequestParam int numQ,@RequestParam String title){
 		System.out.println("In create");
 		return quizservice.createQuiz(category,numQ,title);
+	}
+	
+	@GetMapping("get/{id}")
+	public ResponseEntity<List<QuestionWapper>> getQuiz(@PathVariable Integer id){
+		
+		return quizservice.getQuiz(id);
 	}
 }
--- src/main/java/com/javaTest/questionBank/Service/QuizService.java
+++ src/main/java/com/javaTest/questionBank/Service/QuizService.java
@@ -1,6 +1,8 @@
 package com.javaTest.questionBank.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 import com.javaTest.questionBank.DAO.QuestionDao;
 import com.javaTest.questionBank.DAO.QuizDao;
 import com.javaTest.questionBank.Model.Question;
+import com.javaTest.questionBank.Model.QuestionWapper;
 import com.javaTest.questionBank.Model.Quiz;
 
 @Service
@@ -18,15 +21,26 @@ public class QuizService {
 	QuestionDao questiondao;
 	@Autowired
 	QuizDao quizDao;
-	
+
 	public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
-		List<Question> questions= questiondao.findRandomQuestionsByCategory(category,numQ);
+		List<Question> questions = questiondao.findRandomQuestionsByCategory(category, numQ);
 		Quiz quiz = new Quiz();
 		quiz.setTitle(title);
 		quiz.setQuestions(questions);
 		quizDao.save(quiz);
-		return new ResponseEntity<String>("Success",HttpStatus.CREATED);
+		return new ResponseEntity<String>("Success", HttpStatus.CREATED);
 	}
 
-	
+	public ResponseEntity<List<QuestionWapper>> getQuiz(Integer id) {
+		Optional<Quiz> quiz = quizDao.findById(id);
+		List<Question> queFromDb = quiz.get().getQuestions();
+		List<QuestionWapper> queForUser= new ArrayList<>();
+		for (Question q : queFromDb) {
+			QuestionWapper questionwrapper =  new QuestionWapper( q.getQid(),q.getQuestion(),q.getOption1(),q.getOption2(),q.getOption3(),q.getOption4());
+			queForUser.add(questionwrapper);
+			
+		}
+		return new ResponseEntity<>(queForUser,HttpStatus.OK);
+	}
+
 }
